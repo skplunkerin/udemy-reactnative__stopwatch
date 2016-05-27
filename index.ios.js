a40975162417c72ed76c90597290522172fdb1fb
Christopher Reynolds <reynolds87chris@gmail.com>
2016-05-26 18:25:54 -0600

    - Add mockup image screenshot of how app will look - Add border helper function to display sections of app - Add styles to different section to be correct alignments

--- index.ios.js
+++ index.ios.js
@@ -15,13 +15,56 @@ import {
 import Button from './components/button'
 import Time from './components/time'
 
-var StopWatch = React.createClass({
+let StopWatch = React.createClass({
+  // Helper function to visually assist seeing sections of app
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  },
+
   render: function() {
-    return <View>
-      <Time />
-      <Button text="Start" />
-      <Button text="Lap" />
+    return <View style={styles.container}>
+      <View style={[styles.header, this.border('yellow')]}>
+        <View style={[styles.timeWrapper, this.border('red')]}>
+          <Time />
+        </View>
+        <View style={[styles.buttonWrapper, this.border('green')]}>
+          <Button text="Start" />
+          <Button text="Lap" />
+        </View>
+      </View>
+      <View style={[styles.footer,this.border('blue')]}>
+        <Text>
+          TODO: list of laps
+        </Text>
+      </View>
     </View>
+  }
+})
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1, // Fill the entire screen
+    alignItems: 'stretch' // Each child element to take as much space as can (height)
+  },
+  header: { // Yellow
+    flex: 1
+  },
+  footer: { // Blue
+    flex: 1
+  },
+  timeWrapper: { // Red
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: { // Green
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around', // equally distribute children in space given
+    alignItems: 'center' // equally distribute children in space given
   }
 })
 
